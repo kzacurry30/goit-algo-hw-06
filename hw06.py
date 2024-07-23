6600262ba4618c607293f8ebd901f476130e9470
kzacurry30 <102797063+kzacurry30@users.noreply.github.com>
2024-07-23 22:57:37 +0300

    hw06.py

--- hw06.py
+++ hw06.py
@@ -55,32 +55,39 @@ class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
-    def find_record(self, name):
+    def find(self, name):
         return self.data.get(name)
 
-    def delete_record(self, name):
+    def delete(self, name):
         if name in self.data:
             del self.data[name]
 
     def __str__(self):
         return '\n'.join(str(record) for record in self.data.values())
 
-
+ 
 book = AddressBook()
-
 
 john_record = Record("John")
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
 
-
 book.add_record(john_record)
-
 
 jane_record = Record("Jane")
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
-
 print(book)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)
+  
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")
+
+book.delete("Jane")
+
 
